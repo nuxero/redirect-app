4d56234195ac2ad05a230e9783f8343f96f56e91
Hector Zelaya <inge.zelaya@gmail.com>
2020-06-01 07:26:23 -0600

    added port env

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const target = process.env.TARGET;
 
 app.get("*", (req, res) => res.redirect(301, target));
